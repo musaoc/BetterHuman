eea6f62111f014d4cc696b4f5ac9a8bb5c34f405
Muhammad Musa Khan <musakhanmiz9@gmail.com>
2024-08-04 03:35:58 +0500

    changed the patreon files

--- src/components/Quests.js
+++ src/components/Quests.js
@@ -4,11 +4,13 @@ import React from 'react';
 
 const Quests = () => {
   return (
-    <div style={{ padding: '2rem', color: '#ffffff', backgroundColor: '#23272a', minHeight: '100vh' }}>
+    <div style={{ padding: '2rem', color: '#000000', backgroundColor: '#ffffff', minHeight: '100vh' }}>
       <h1>Quests</h1>
-      <p>Sample text for the quests page.</p>
+      <p>This app has just been launched and we don't plan to post up ads or clickbaits to earn money. It will be you who will help us in keeping our mission alive. </p>
+      <p>We require support to keep working on BetterHuman. As you can see our UI/UX is not developed and we don't have a community </p>
+      <h3>So, join us in this journey to help people, in becoming better, becoming stronger.</h3>
       <div>
-        <a href="https://www.patreon.com/musaoc/membership">Your Patreon Link Here</a>
+        <a href="https://www.patreon.com/musaoc/membership">Our Patreon Link </a>
       </div>
     </div>
   );
